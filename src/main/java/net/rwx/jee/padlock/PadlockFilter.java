99adac262c3d05f3b9cdd5598012babe9cec0269
Arnaud Fonce <arnaud.fonce@r-w-x.net>
2016-07-26 18:38:36 +0200

    Try to reuse jaxrs param converters (but so far no luck)

--- src/main/java/net/rwx/jee/padlock/PadlockFilter.java
+++ src/main/java/net/rwx/jee/padlock/PadlockFilter.java
@@ -5,29 +5,38 @@
  */
 package net.rwx.jee.padlock;
 
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.lang.reflect.Method;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
-import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Providers;
 import org.jose4j.jws.AlgorithmIdentifiers;
 import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jwt.JwtClaims;
@@ -65,7 +74,7 @@ public class PadlockFilter implements ContainerRequestFilter, ContainerResponseF
         try {
             if (needAuthentication()) {
                 readJWTCookie(requestContext);
-                checkAuthorization();
+                checkAuthorization(requestContext);
             }
         } catch (UnauthorizedException ue) {
             unauthorized(requestContext);
@@ -144,16 +153,56 @@ public class PadlockFilter implements ContainerRequestFilter, ContainerResponseF
         }
     }
 
-    private void checkAuthorization() {
-//        Authorization auth = beanManager.getBeans(Authorization.class).iterator().next();
+    @Context
+    private HttpServletRequest servletRequest;
+    
+    @Context
+    private Application application;
+    
+    private void checkAuthorization(ContainerRequestContext requestContext) throws IntrospectionException {
+        System.out.println("############### #################### ################ " + converters.);
         Authorization authorization = resourceInfo.getResourceMethod().getAnnotation(Authorization.class);
         if( authorization != null ) {
-//            CDI.current()
-//            Bean<?> authorizationChecker = beanManager.getBeans(authorization.value()).iterator().next();
-            
+            Bean<?> bean = beanManager.resolve(beanManager.getBeans(authorization.value()));
+            Object authorizationChecker = beanManager.getReference(bean, bean.getBeanClass(), beanManager.createCreationalContext(bean));
+            System.out.println("############################ " + authorizationChecker) ;
+            for( AuthorizationParameter authParameter : resourceInfo.getResourceMethod().getAnnotation(AuthorizationParameters.class).value() ) {
+                System.out.println("############################### " + authParameter.value());
+                for( PropertyDescriptor descriptor : Introspector.getBeanInfo(authorization.value()).getPropertyDescriptors() ) {
+                    System.out.println("########## " + descriptor.getName() );
+                    if( descriptor.getName().equals(authParameter.value()) ) {
+                        Method writeMethod = descriptor.getWriteMethod();
+                        System.out.println("##################### ##### " + requestContext.getUriInfo().getPathParameters().getFirst(authParameter.value()));
+//                        writeMethod.invoke(authorizationChecker, args);
+                    }
+                }
+                
+            }
         }
     }
 
+//    private Object getValueForAuthorizationParameter(AuthorizationParameter parameter, ContainerRequestContext requestContext) {
+//        Annotation[][] annotations = resourceInfo.getResourceMethod().getParameterAnnotations();
+//        for( int iMethodParameter=0; iMethodParameter < annotations.length; iMethodParameter++ ) {
+//            for( Annotation annotation : annotations[iMethodParameter] ) {
+//                if( annotation instanceof PathParam ) {
+//                    if( ((PathParam)annotation).value().equals(parameter.value()) ) {
+//                        return convertValueToParameterType(
+//                                requestContext.getUriInfo().getPathParameters().getFirst(parameter.value()),
+//                                resourceInfo.getResourceMethod().getParameterTypes()[iMethodParameter]);
+//                    }
+//                }
+//            }
+//        }
+//        
+//    }
+    
+    @Context
+    private Providers providers;
+    
+    @Inject
+    private Provider<ParamConverterProvider> converters;
+    
     private class UnauthorizedException extends Exception {
 
     }
